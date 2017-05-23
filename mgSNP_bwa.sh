061dcfe4c073a99d500b960de5fe70467140361b
Ranjit Kumar <ranjit58@gmail.com>
2017-05-23 16:15:20 -0500

    first commit

--- mgSNP_bwa.sh
+++ mgSNP_bwa.sh
@@ -13,8 +13,10 @@ READGROUP="@RG\tID:G${SAMPLE}\tSM:${SAMPLE}\tPL:Illumina\tLB:lib1\tPU:unit1"
 
 # alignment using BWA MEM with -M and -R options
 #bwa mem -M -t $THREAD -R $READGROUP $REF ../RAWDATA_QC/${SAMPLE}_F.fastq ../RAWDATA_QC/${SAMPLE}_R.fastq > ${SAMPLE}.sam
+
 #use if trimmomatic is used
-#bwa mem -M -t $INTHREAD -R $READGROUP $REF ../RAWDATA_QC/${SAMPLE}_F_P.fastq ../RAWDATA_QC/${SAMPLE}_R_P.fastq > ${SAMPLE}.sam
-bwa mem -M -t $THREAD -R $READGROUP ${REF} ${RAWDATA}/${SAMPLE}_F.fastq ${RAWDATA}/${SAMPLE}_R.fastq > $BWA_FILES/${SAMPLE}.sam
+bwa mem -M -t $INTHREAD -R $READGROUP $REF ${RAWDATA}/${SAMPLE}_F_P.fastq ${RAWDATA}/${SAMPLE}_R_P.fastq > $BWA_FILES/${SAMPLE}.sam
+
+#bwa mem -M -t $THREAD -R $READGROUP ${REF} ${RAWDATA}/${SAMPLE}_F.fastq ${RAWDATA}/${SAMPLE}_R.fastq > $BWA_FILES/${SAMPLE}.sam
 
 echo -e "\nINFO: BWA alignment complete for sample ${SAMPLE}"
